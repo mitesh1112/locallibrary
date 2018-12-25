7fc78cf76ed23db1126868b7e467f078ef3731a9
Mitesh Budhabhatti <mitesh.budhabhatti@gmail.com>
2018-12-25 16:55:22 +0530

    Completed - skeleton_website

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+def home(request):
+    html = '<h1>Mitesh</h1>'
+    return HttpResponse(html)
