ec9e43752ef24d786e200f73c834185e1a7cd7a2
Mitesh Budhabhatti <mitesh.budhabhatti@gmail.com>
2018-12-28 19:50:26 +0530

    Completed Home_Page

--- catalog/views.py
+++ catalog/views.py
@@ -2,7 +2,27 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
-def home(request):
-    html = '<h1>Mitesh</h1>'
-    return HttpResponse(html)
+from catalog.models import Author, Book, BookInstance, Genre
+
+def index(request):
+    """View function for the homepage of the site"""
+
+    # Generate count of some of the main objects
+    num_book = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact = 'a').count()
+
+    # The 'all()' is implied by default
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_book' : num_book,
+        'num_instances' : num_instances,
+        'num_instances_available' : num_instances_available,
+        'num_authors' : num_authors
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
